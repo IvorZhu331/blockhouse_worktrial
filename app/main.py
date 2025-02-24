725b51db14ef5ea693fdeb64a7c62f51d09bf7a0
Ivor Zhu <sz3833@nyu.edu>
2025-02-23 23:04:58 -0500

    minor fix

--- app/main.py
+++ app/main.py
@@ -17,4 +17,4 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="54.163.82.225", port=8000, reload=True)
